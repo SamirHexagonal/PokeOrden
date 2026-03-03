ff6e4206f58cdf710487a5001e84dbd04cd3a8c6
SamirItaca <0260scabrera@e-itaca.es>
2026-03-03 20:41:15 +0100

    tmp

--- backend/server.js
+++ backend/server.js
@@ -9,29 +9,8 @@ app.use(cors());
 app.use(express.json());
 // FIN CONFIG
 
-// PAGINAS
-const path = require('path');
-
-// Carpeta pages
-const DIR_PAGES = path.join(__dirname, 'public/pages');
-
-// Página principal → login
-app.get('/', (req, res) => {
-    res.sendFile(path.join(DIR_PAGES, 'login.html'));
-});
-
-app.get('/:page', (req, res) => {
-    const file = path.join(DIR_PAGES, req.params.page + '.html');
-    res.sendFile(file);
-});
-
-// Servir archivos estáticos (css, js, imágenes)
-app.use(express.static(path.join(__dirname, 'public')));
-
-// FIN PAGINAS
-
 // ENDPOINTS
-app.use('/api/books', require('./routes/books'));
+//app.use('/api/books', require('./routes/books'));
 // FIN ENDPOINT
 
 const PORT = process.env.PORT || 3000;
